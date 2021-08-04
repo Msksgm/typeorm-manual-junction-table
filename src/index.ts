54d5d064a3c2d420e43e8af5c11c73060da4d28a
Msksgm <msksgm3124@gmail.com>
2021-08-04 20:57:31 +0900

    add: dbへの挿入処理を追加

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,66 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
 import { User } from "./entity/User";
+import { Work } from "./entity/Work";
+import { UserWorks } from "./entity/UserWorks";
 
 createConnection()
   .then(async (connection) => {
     console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
+    const user1 = new User();
+    user1.firstName = "hoge";
+    user1.lastName = "hoge";
+    user1.age = 25;
+    await connection.manager.save(user1);
 
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
+    const user2 = new User();
+    user2.firstName = "fuga";
+    user2.lastName = "fuga";
+    user2.age = 25;
+    await connection.manager.save(user2);
+
+    const user3 = new User();
+    user3.firstName = "piyo";
+    user3.lastName = "piyo";
+    user3.age = 35;
+    await connection.manager.save(user3);
+
+    console.log("Inserting a new work into the database...");
+    const work1 = new Work();
+    work1.title = "フロント";
+    work1.body = "運用";
+    await connection.manager.save(work1);
+
+    const work2 = new Work();
+    work2.title = "バックエンド";
+    work2.body = "API作成";
+    await connection.manager.save(work2);
+
+    const work3 = new Work();
+    work3.title = "DB";
+    work3.body = "ER設計";
+    await connection.manager.save(work3);
+
+    console.log("Inserting a new userWork into the database...");
+    const userWork1 = new UserWorks();
+    userWork1.user_id = 1;
+    userWork1.work_id = 1;
+    await connection.manager.save(userWork1);
+
+    const userWork2 = new UserWorks();
+    userWork2.user_id = 1;
+    userWork2.work_id = 2;
+    await connection.manager.save(userWork2);
+
+    const userWork3 = new UserWorks();
+    userWork3.user_id = 2;
+    userWork3.work_id = 1;
+    await connection.manager.save(userWork3);
+
+    const userWork4 = new UserWorks();
+    userWork4.user_id = 3;
+    userWork4.work_id = 3;
+    await connection.manager.save(userWork4);
 
     console.log("Here you can setup and run express/koa/any other framework.");
   })
